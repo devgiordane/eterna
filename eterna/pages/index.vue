40a0f5ae916b8c0ba7dd3f7ead935be72e629d61
devgiordane <bruno_giordane@outlook.com>
2024-09-21 18:58:15 -0300

    Versão inacaba, backup

---
+++ eterna/pages/index.vue
@@ -0,0 +1,235 @@
+<template>
+    <div class="">
+        <div class="hero bg-base-100 min-h-screen" style="background-image: url('fundo.jpg');">
+            <div class="hero-overlay bg-opacity-50 "></div>
+            <div class="hero-content text-center">
+                <div class="max-w-md text-white">
+                    <h1 class="text-5xl font-bold">Clinica Eterna Pro</h1>
+                    <p class="py-6"> A Clínica Eterna está localizada no Ed. Lagoa Center, em Natal (RN) e oferece
+                        diversos tratamentos especializados em Fisioterapia dermatofuncional, estética, botox, laser,
+                        harmonização facial, estética íntima, terapia manual e osteopatia, com atendimento
+                        fisioterapêutico individualizado. </p> <a href="https://maps.app.goo.gl/nURTXJkJdCCDPbjj6"
+                        target="_blank"
+                        class="btn btn-warning text-white bg-orange-300 border-none text-2xl font-thin w-full">
+                        <MapPinned /> Como Chegar
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="container mx-auto px-4">
+            <div class="py-10">
+                <h1 class="text-3xl font-bold">Serviços</h1>
+                <p>A Clínica Eterna Pro oferece tratamentos para: </p>
+                <div class="grid grid-cols-2 gap-5 py-10"> <span
+                        class="text-xl p-5 flex justify-center from-slate-200 to-orange-50 bg-gradient-to-tr items-center rounded-lg shadow-md">Pós-operatório
+                        de cirurgias plasticas</span> <span
+                        class="text-xl p-5 flex justify-center from-slate-200 to-orange-50 bg-gradient-to-tr items-center rounded-lg shadow-md">Celulite</span>
+                    <span
+                        class="text-xl p-5 flex justify-center from-slate-200 to-orange-50 bg-gradient-to-tr items-center rounded-lg shadow-md">Estrias</span>
+                    <span
+                        class="text-xl p-5 flex justify-center from-slate-200 to-orange-50 bg-gradient-to-tr items-center rounded-lg shadow-md">Gordura
+                        localizada</span> <span
+                        class="text-xl p-5 flex justify-center from-slate-200 to-orange-50 bg-gradient-to-tr items-center rounded-lg shadow-md">Flacidez</span>
+                    <span
+                        class="text-xl p-5 flex justify-center from-slate-200 to-orange-50 bg-gradient-to-tr items-center rounded-lg shadow-md">Fisioterapia
+                        Pélvica</span> <span
+                        class="text-xl p-5 flex justify-center from-slate-200 to-orange-50 bg-gradient-to-tr items-center rounded-lg shadow-md">Limpeza
+                        e hidratação da pele</span> <span
+                        class="text-xl p-5 flex justify-center from-slate-200 to-orange-50 bg-gradient-to-tr items-center rounded-lg shadow-md">Rugas
+                        e envelhecimento da pele</span>
+                </div>
+            </div>
+            <div class="py-10">
+                <h1 class="text-3xl font-bold pb-10">Pré e pós-operatório em cirurgia plástica</h1>
+                <p>O atendimento de PRÉ E PÓS-OPERATÓRIO EM CIRURGIA PLÁSTICA se destaca na Eterna Pro, pois sua equipe
+                    conta com grande quantidade de equipamentos para este tipo de assistência, além de possuir uma longa
+                    experiência nesta área, o que faz desta clínica uma referência em Fisioterapia Dermato-funcional na
+                    cidade de Natal. </p>
+                <p> Desde suas instalações até o acesso para o paciente já foi estruturado para uma maior comodidade
+                    deste público. </p>
+                <h3 class="text-xl font-medium py-10"> Contamos também com outros tratamentos diferenciados destinados a
+                    outras especialidades: </h3>
+                <div class="grid grid-cols-1 gap-5"> <span class="flex gap-x-2">
+                        <DiamondPlus /> Tratamentos para o pré e pós-parto
+                    </span> <span class="flex gap-x-2">
+                        <DiamondPlus /> Assistência na incontinência urinária
+                    </span> <span class="flex gap-x-2">
+                        <DiamondPlus /> Distúrbios vasculares e pós cirúrgico de varizes
+                    </span> </div>
+                <h1 class="text-3xl font-bold py-10">Pós-operatório de cirurgias plásticas </h1>
+                <p>Com mais de 30 anos de experiência em pós-operatórios de cirurgias plásticas e contando com a
+                    experiência da Dra. Patricia Froes, e Clínica Eterna Pro é referência pós-operatório em Natal. </p>
+                <h1 class="text-3xl font-bold py-10">Pós-Estética Facial </h1>
+                <p>A estética facial é o conjunto de procedimentos que envolve a harmonia, a simetria e a aparência da
+                    face. Seus tratamentos promovem um melhor impacto estético, proporcionando assim melhorias não só a
+                    autoestima, mas também a qualidade de vida. </p>
+                <h1 class="text-3xl font-bold py-10">Estética corporal </h1>
+                <p>A estética corporal se trata do conjunto de procedimentos voltados à melhor aparência do físico do
+                    corpo e melhor aparência da pele. Entre as soluções que a estética corporal é capaz de proporcionar
+                    estão a maior suavidade da pele, o tratamento da flacidez e o combate da gordura localizada. </p>
+                <h1 class="text-3xl font-bold py-10">Anti-Aging </h1>
+                <p>O Anti-aging retarda o envelhecimento e promove qualidade de vida aos seus pacientes, muitos reclamam
+                    de dores e doenças que podem ser facilmente tratadas com os tratamentos indicados por um
+                    profissional. </p>
+            </div>
+            <div class="">
+                <div class="hero bg-base-200 min-h-screen rounded-xl">
+
+                    <div class="hero-content text-center">
+                        <div class="max-w-md">
+                            <h1 class="text-4xl py-10">Conheça nossa equipe
+                            </h1>
+                            <img src="@/assets/patricia.jpg" class="rounded-full border-white border-4 h-96  w-auto"
+                                alt="">
+                            <h3>Dr. Patrícia Froes</h3>
+                            <p class="py-6"> Patricia Froes é a fundadora e CEO da Clínica Eterna, com mais de 30 anos
+                                de carreira dedicados ao estudos científicos, traz consigo experiência e embasamento
+                                cientifico para fazer da Clínica Eterna referencia em tratamento de qualidade
+                                comprovada.
+
+
+                                <br><br>
+                                Confira seu currículo completo abaixo.
+
+                            </p> <a href="https://maps.app.goo.gl/nURTXJkJdCCDPbjj6" target="_blank"
+                                class="btn btn-warning text-white bg-orange-300 border-none text-2xl font-thin w-full">
+                                <School /> Curriculum
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="grid md:grid-cols-3 grid-cols-1 gap-2  py-10">
+                    <div class="card bg-base-100 shadow-xl">
+                        <figure>
+                            <img src="@/assets/marcel.jpg" />
+                        </figure>
+                        <div class="card-body p-2 pb-10">
+                            <h2 class="card-title">Rodrigo Marcel Valentim
+                            </h2>
+                            <p>Doutor em Fisioterapia</p>
+
+                            <ul class="px-3">
+
+                                <li class="flex">
+                                    <Dot /> Doutor em Fisioterapia pela Universidade Federal do Rio Grande do Norte
+                                </li>
+                                <li class="flex">
+                                    <Dot /> Docente da graduaçãoe pós graduaçãoem fisioterapia
+                                </li>
+                                <li class="flex">
+                                    <Dot /> Membro do International Research Group – IRG
+                                </li>
+                                <li class="flex">
+                                    <Dot /> Diplomado em fisioterapia dermato funcional ( Argentina , Chile e Espanha
+                                    🇪🇸)
+                                </li>
+                                <li class="flex">
+                                    <Dot /> Formação em osteopatia
+                                </li>
+                                <li class="flex">
+                                    <Dot /> Formação no método pilates
+                                </li>
+                                <li class="flex">
+                                    <Dot /> Formação em Microfisioterapia
+                                </li>
+                                <li class="flex">
+                                    <Dot /> Formação em Microeletrolises percutânea
+                                </li>
+                                <li class="flex">
+                                    <Dot /> Formação em abordagem terapêutica biológica
+                                </li>
+                                <li class="flex">
+                                    <Dot /> Pesquisador na área da fisioterapia ( + 100 artigos publicados em periódicos
+                                    nacionais e
+                                    internacionais)
+                                </li>
+                            </ul>
+
+                        </div>
+                    </div>
+                    <div class="card bg-base-100 shadow-xl">
+                        <figure>
+                            <img src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                                alt="Shoes" />
+                        </figure>
+                        <div class="card-body p-2 pb-10">
+                            <h2 class="card-title">Laryssa Vasconcelos
+                            </h2>
+                            <p>Fisioterapeuta</p>
+
+                            <ul class="px-3">
+
+                                <li>
+                                    Bacharel em Fisioterapia pelo Centro Universitário do Rio Grande do Norte (UNI-RN) e
+                                    pós-graduanda em Saúde da Mulher pela Faculdade de Ciências Médicas de Minas Gerais
+                                    (FCMMG)
+                                </li>
+
+                                <li>
+                                    Serviços:
+                                </li>
+                                <ul class="pl-10">
+                                    <li class="list-disc">Pós-operatório facial e corporal;</li>
+                                    <li class="list-disc">Estética facial e corporal, com tratamentos para flacidli,
+                                        gordura localizada, dentre outras patologias estéticas;
+                                    </li>
+                                    <li class="list-disc">Drenagem Linfática Manual Facial e corporal (Métlio
+                                        convencional, Joana Medrado ou Renata França);
+                                    </li>
+                                    <li class="list-disc">Massagem Modeladora (Método convencional ou Renata Françli;
+                                    </li>
+                                    <li class="list-disc">Treinamento Hipopressivo pelo Método Low Pressure Fitneli;
+                                    </li>
+                                    <li class="list-disc">Tratamento para diástase.</li>
+                                </ul>
+                            </ul>
+
+                        </div>
+                    </div>
+                    <div class="card bg-base-100 shadow-xl">
+                        <figure>
+                            <img src="@/assets/marcel.jpg" />
+                        </figure>
+                        <div class="card-body p-2 pb-10">
+                            <h2 class="card-title">Lígia Revorêdo
+                            </h2>
+                            <p>Fisioterapeuta Pélvica e Dermatofuncional</p>
+                            <ul class="px-3">
+
+                                <li>
+                                    Bacharel em Fisioterapia pelo Centro Universitário do Rio Grande do Norte (UNI-RN) e
+                                    pós-graduanda em Osteopatia pela Escuela de Osteopatia de Madrid.
+
+
+                                </li>
+
+                                <li>
+                                    Serviços:
+                                </li>
+                                <ul class="pl-10">
+                                    <li class="list-disc">Pós-operatório facial e corporal;</li>
+                                    <li class="list-disc">Formação em terapia mecanomoduladora em tecidos cicatriciais
+                                        (Instituto Mariane Altomare);
+
+                                    </li>
+                                    <li class="list-disc">Terapia manual para disfunções musculoesqueléticas;
+
+                                    </li>
+                                    <li class="list-disc">Estética facial e corporal, com tratamentos para flacidez,
+                                        gordura localizada, dentre outras patologias estéticas;
+                                    </li>
+                                    <li class="list-disc">Drenagem Linfática Manual facial e corporal;
+                                    </li>
+                                    <li class="list-disc">Treinamento Hipopressivo pelo Método MAH, com base na
+                                        metodologia nas colunas de pressão de Williame e Finet.</li>
+                                </ul>
+                            </ul>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script setup lang="ts">import { DiamondPlus, Dot, MapPinned, School } from 'lucide-vue-next';</script>
